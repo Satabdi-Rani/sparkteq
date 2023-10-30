ee4d1c7b83451db4cca80fc42c9ec3666a91ff57
Satabdi Rani <32415291+Satabdi-Rani@users.noreply.github.com>
2023-10-30 16:09:38 +0600

    Animation added

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,8 +5,9 @@ const Banner = () => {
     return (
         <div style={{backgroundImage: `url(${banner})`}} className='w-full h-[40%] md:block content-div shadow-lg  rounded-md pt-12 sm:pt-24 mx-auto'>
             <div className='flex flex-col justify-center items-center'>
-                <h1 className='text-[#222249] text-4xl md:text-5xl lg:text-6xl font-extrabold text-center'>The Power of Technology</h1>
-                <h4 className='text-purple-600 text-md sm:text-lg font-medium w-[90%] md:w-[70%] lg:w-[50%] mx-auto text-center mt-6'>Technology empowers us to innovate, communicate, and navigate the complexities of the modern age</h4>
+                <h1 className='text-[#222249] text-4xl md:text-5xl lg:text-6xl font-extrabold text-center animate-fade-in'>The Power of Technology</h1>
+                <h4 className='text-purple-600 text-md sm:text-lg xl:text-xl font-medium w-[90%] md:w-[70%] lg:w-[50%] mx-auto text-center mt-6 
+                animate-slide-in '>Technology empowers us to innovate, communicate, and navigate the complexities of the modern age</h4>
                 <button className='px-4 py-2 text-md bg-purple-600 rounded text-white mt-4'>Know More</button>
             </div>
         </div>
